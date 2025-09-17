20bd0506e3f4a26c759181413fa866b67ba3b9c7
HerrPhi <fingatta@gmail.com>
2025-09-17 11:09:13 +0200

    code now works: added get_files_info tests.py (made them work) (calculator and its directorys and programms were added in the previous (nor working) commit)

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,16 +4,21 @@ def get_files_info(working_directory, directory="."):
     full_path_pre = os.path.join(working_directory, directory)
     full_path = os.path.abspath(full_path_pre)
     #full_path.startswith(working_directory)
-    if working_directory not in full_path:
+    abs_work = os.path.abspath(working_directory)
+    if not (abs_work == full_path or full_path.startswith(abs_work + os.sep)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory'
     
     files = os.listdir(full_path)
+    result_list = []
     result_string = ""
     for file in files:
         try:
-            result_string += f"- {file} {os.path.getsize(file)}, {os.path.isdir(file)}" + "\n"
+            file_path = os.path.join(full_path, file)
+            result_list.append(f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
+            #- {file}: file_size={size} bytes, is_dir={is_dir}
+            result_string = "\n".join(result_list)
         except Exception as e:
             return f"Error: {e}"
         
